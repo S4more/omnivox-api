406f02558677bb2c31333dedbe2cac897197cd25
S4more <guimsc@hotmail.com>
2021-12-21 15:55:49 -0500

    Get documents working

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import {login} from "./requester";
+import {login, loadMio} from "./requester";
 import axios, {AxiosResponse} from 'axios';
 import { wrapper } from 'axios-cookiejar-support';
 import { CookieJar } from 'tough-cookie';
+import qs from 'qs';
+//import fetch from 'node-fetch';
 
 const website: string = "https://dawsoncollege.omnivox.ca";
 const jar = new CookieJar();
 const client = wrapper(axios.create({jar}));
 
+
 async function getData() {
+//    const response = await fetch(website);
+//    console.log(response);
+
     try {
         // I'm using two HTML request libraries here because I was testing.
         // axios wouldn't work for the cache stuff so I just moved to request.
@@ -19,8 +25,13 @@ async function getData() {
         const k = answer.substring(init, init + 18);
 
         const cookie_string = jar.getCookieStringSync(website + "/intr/Module/Identification/Login/Login.aspx");
-        let cookie = await login(k, cookie_string, "user", "password");
-        console.log(cookie);
+        let cookie = await login(k, cookie_string, "username", "password");
+
+        let cookie_str = cookie.join("; ");
+
+        loadMio(cookie_str);
+
+
         //headers['cookie'] = cookie.join(" ");
 
     } catch (error) {
--- src/requester.ts
+++ src/requester.ts
@@ -61,6 +61,39 @@ export async function login(k: string, cookie: string, id: string, password: str
     return promise;
 }
 
-function loadMio() {
+export function loadMio(cookie: string) {
+    const options: any = {
+      'method': 'GET',
+      'url': 'https://dawsoncollege.omnivox.ca/intr/webpart.ajax?IdWebPart=00000000-0000-0000-0003-000000000008&L=ENG',
+      'headers': {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded',
+        'authority': 'dawsoncollege.omnivox.ca',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Chromium";v="95", ";Not A Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'upgrade-insecure-requests': '1',
+        'origin': 'https://dawsoncollege.omnivox.ca',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://dawsoncollege.omnivox.ca/Login/Account/Login?ReturnUrl=%2fintr',
+        'accept-language': 'en-GB,en;q=0.9',
+        'host': 'dawsoncollege.omnivox.ca',
+        'connection': 'close',
+        'cookie': cookie
+      }
+    }
+    request(options, (_: any, response: Response) => {
+        let body: string = response.body;
+        const left = body.search("xmlns=\"\"");
+        body = body.substring(left + "xmlns=\"\">".length);
+        body = body.substring(0, body.search("</title>"));
+        console.log(body);
+
+    });
 }
 
