79d0274dd94f692a2a3aaa7946194d9e06aae4f8
Noah <noah.labrecue@gmail.com>
2021-12-26 10:56:16 -0500

    Added login route

--- omni-client/src/router/index.ts
+++ omni-client/src/router/index.ts
@@ -10,10 +10,12 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
 ]
 
