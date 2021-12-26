93f5d72946aef1200d84892e53abd5172659ce42
Noah <noah.labrecue@gmail.com>
2021-12-26 10:56:37 -0500

    started login page

---
+++ omni-client/src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="login">
+    <img alt="Omni Logo" src="../assets/logo.png">
+    <section class="error" v-if="hasError">
+      <p class="errorMsg">
+        {{errorMsg}}
+      </p>
+    </section>
+    <section class="inputs">
+      <input type="text" v-model="username">
+      <input type="password" v-model="password">
+      <button @click="login">Login</button>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+
+const data = {
+  hasError:false,
+  errorMsg:"",
+  password: "",
+  username: ""
+}
+
+@Options({
+  components: {},
+  data: function() {
+    return data;
+  },
+  methods: {
+    login() {
+      // TOOD Handle login
+    },
+
+    error(message:string) {
+      this.hasError = true;
+      this.errorMsg = message;
+    }
+  }
+})
+
+export default class Login extends Vue {};
+
+</script>
