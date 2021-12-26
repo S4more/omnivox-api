4b40b25d04402c5882fdb2a3b030b2c579030642
Noah <noah.labrecue@gmail.com>
2021-12-26 10:55:53 -0500

    Changed styling of navbar

--- omni-client/src/App.vue
+++ omni-client/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link class="home" to="/"><img class="homeIcon" src="./assets/logo.svg"></router-link>
     <router-link to="/about">About</router-link>
+    <router-link to="/login">Login</router-link>
   </div>
   <router-view/>
 </template>
@@ -15,15 +16,46 @@
   color: #2c3e50;
 }
 
+body {
+  background-color: #f3f3f3;
+}
+
 #nav {
-  padding: 30px;
+  display: flex;
+  background-color: #fff;
+  justify-content: center;
+  align-items:center;
+  box-shadow: 0px 5px 5px #0005;
+  border-radius: 0.25rem;
+  height: 2.5rem;
+
+  > * {
+    margin: 0.5rem;
+  }
+
+  > .home {
+    margin: 0rem;
+  }
+
+  .homeIcon {
+    width: 1.6rem;
+    height: 1.6rem;
+  }
 
   a {
     font-weight: bold;
     color: #2c3e50;
-
+    text-decoration: none;
+    height: 1.5rem;
+    line-height: 1.5rem;
+    vertical-align: middle;
+    border-bottom: 0.2rem solid #0000;
+    padding: 0.5rem;
+    &:hover{
+      background-color: #0002;
+    }
     &.router-link-exact-active {
-      color: #42b983;
+      border-bottom: 0.2rem solid #00E0FF;
     }
   }
 }
