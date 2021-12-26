36ec3f887442442f65dff735ba3932ac9545d81c
Mathews-Velez <velezmathew@gmail.com>
2021-12-26 01:03:07 -0500

    finished login prompt

--- crawler/src/utils/loginPromt.ts
+++ crawler/src/utils/loginPromt.ts
@@ -1,70 +1,46 @@
-
 import console from "console";
 
-async function getId() {
-    const { AuthPrompt } = require('enquirer');
+async function getCred() {
+    const { prompt } = require('enquirer');
+    const question = [
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your student ID?'
+        },
+        {
+            type: 'password',
+            name: 'password',
+            message: 'What is your password?'
+        }
+    ];
+    let answers = await prompt(question);
+    //concatinate the two strings into one
+    let str = "user_name=" + answers["username"] + "\npassword=" + answers["password"]
+    return str;
 
-    function authenticate(_value: any, _state: any) {
-        return true;
-    }
-
-    const CustomAuthPrompt = AuthPrompt.create(authenticate);
-
-    const prompt = new CustomAuthPrompt({
-        name: 'password',
-        message: 'Please enter your password',
-        username: 'rajat-sr',
-        password: '1234567',
-        choices: [
-            { name: 'username', message: 'username' },
-            { name: 'password', message: 'password' }
-        ]
-    });
-
-    prompt
-        .run()
-        .then((answer: any) => console.log('Authenticated?', this.username))
-        .catch(console.error);
 }
 
-export async function getStdntCred() {
-    let studentID = getId()
-    console.log(studentID)
-}
-
-export function credExist() {
-    //check if there is already a .env
+export async function loginUser() {
     const fs = require("fs")
+    //check if there is already a .env
     let exists = fs.existsSync("./.env")
+
     //ask user for their login info and write to .env file
     if (!exists) {
+        console.log(".env file not found")
+        let cred = await getCred()
+        const fs = require('fs')
 
+        fs.writeFile('./.env', cred, (err: any) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            //file written successfully
+            console.log(".env file written successfully")
+        })
+    }else{
+        console.log(".env file found")
     }
-    //run the rest of the script
-    loadMio()
 }
-
-// export async function loginUser() {
-//     while (await checkCreds() == false) {
-//         console.log('User credentials were invalid please check and try again')
-//     }
-//     console.log('User credentials validated\n')
-//     loadMio();
-
-// }
-
-// async function checkCreds() {
-//     const { BasicAuth } = require('enquirer');
-//     const prompt = new BasicAuth({
-//         name: 'password',
-//         message: 'Please enter your student ID',
-//         username: '1912607',
-//         password: 'Mateodawson0831',
-//         showPassword: false
-//     });
-//     return (prompt
-//         .run()
-//         .then((answer: boolean) => answer)
-//         .catch(console.error))
-
-// }
