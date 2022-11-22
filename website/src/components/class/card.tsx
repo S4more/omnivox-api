e53da5f427f67f6a50d36f8eaf6d0c135940e71a
S4more <guimsc@hotmail.com>
2022-11-21 21:56:16 -0500

    Possible fix for login -> loading.

--- website/src/components/class/card.tsx
+++ website/src/components/class/card.tsx
@@ -1,11 +1,27 @@
+import { For, Show } from "solid-js";
 import styles from "./card.module.scss"
 export interface ClassCardProps {
-  teacher: string;
   name: string;
+  grade?: string;
+  teacher: string;
   distributedDocuments: number;
+  schedule: string[];
 }
 
-export default function ClassCard({teacher, name, distributedDocuments}: ClassCardProps) {
+export default function ClassCard({teacher, name, distributedDocuments ,grade, schedule}: ClassCardProps) {
+
+  const formatDate = (arr: string[]) => {
+    let finalArr = {} ;
+    for (let i = 0; i < arr.length; i++) {
+      let name = arr[i].slice(0, 3);
+      if (!finalArr[name]) {
+        finalArr[name] = arr[i].slice(4);
+      } else {
+        finalArr[name] += " - " + arr[i].slice(4);
+      }
+    }
+    return Object.keys(finalArr).map(day => `${day}`);
+  }
   return (
   <div class={styles["cardWrapper"]}>
     <div class={styles["title"]}>
@@ -13,9 +29,16 @@ export default function ClassCard({teacher, name, distributedDocuments}: ClassCa
       <p class={styles["sub"]}> { teacher } </p>
     </div>
     <div class={styles["body"]}>
-      <h2 class={styles["relevant"]}> {distributedDocuments} </h2>
-      <text class={styles["new"]}> new </text>
-      <text> documents </text>
+      <div>
+        <h2 class={styles["relevant"]}> {distributedDocuments} </h2>
+        <text class={styles["new"]}> new </text>
+        <text> documents </text>
+      </div>
+      <Show when={grade}>
+        <div>
+          <text> Grade: {grade} </text>
+        </div>
+      </Show>
     </div>
   </div>
   )
--- website/src/routes/dashboard/index.tsx
+++ website/src/routes/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createResource, For, Show } from "solid-js";
+import { createEffect, createResource, createSignal, For, on, Show } from "solid-js";
 import { createRouteAction, Title, useRouteData } from "solid-start";
 import ClassCard from "~/components/class/card";
 import { useLeaContext } from "~/components/provider/omnivox-provider"
@@ -7,7 +7,8 @@ import styles from "./dashboard.module.scss";
 
 export function routeData() {
   const [classes] = createResource(async () => {
-    const resp = await query("LeaClass", "LeaClass", {search: "anres", lookUpType: "CODE"}, ["code", "title", "teacher", "distributedDocuments"]);
+    const resp = await query("LeaClass", "LeaClass", {search: "anres", lookUpType: "CODE"},
+      ["code", "title", "teacher", "distributedDocuments", "grade", "schedule"]);
     console.log("Resp: " + resp);
     return resp
   })
@@ -24,23 +25,34 @@ export default function Dashboard({}: DashboardProps) {
   const [getleaContext, _] = useLeaContext();
   console.log("Token: " + getleaContext.omnivoxToken());
   const { classes } = useRouteData<ReturnType<typeof routeData>>();
+  const [getClasses, setClasses] = createSignal([]);
 
-  createEffect(() => {
-    console.log(classes());
-  });
+  createEffect(() => console.log(classes()));
+
+  createEffect(on(classes, courses => {
+    setClasses(courses);
+  }));
 
   return (
     <>
-    <p> Loading: {classes.loading} </p>
-    <div class={styles.dashboardWrapper}>
-      <For each={classes().sort((a, b) => b.distributedDocuments - a.distributedDocuments)}>
-        {(course) => 
-          <Show when={course.distributedDocuments > 0}>
-            <ClassCard teacher={course.teacher} name={course.title} distributedDocuments={course.distributedDocuments}/>
-          </Show>
-        }
-      </For>
-    </div>
+    <Show when={classes()}>
+      <p> Loading: {classes.loading} </p>
+        <div class={styles.dashboardWrapper}>
+          <For each={classes().sort((a, b) => b.distributedDocuments - a.distributedDocuments)}>
+            {(course) => 
+              <Show when={course.distributedDocuments > 0}>
+                <ClassCard
+                  teacher={course.teacher}
+                  name={course.title}
+                  distributedDocuments={course.distributedDocuments}
+                  grade={course.grade}
+                  schedule={course.schedule}
+                  />
+              </Show>
+            }
+          </For>
+        </div>
+      </Show>
     </>
   )
 }
